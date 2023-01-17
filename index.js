0589d10084e377fca2219ef078ebc9f5f472c64d
SUNDUS-THE-CODER <119600559+SUNDUS-THE-CODER@users.noreply.github.com>
2023-01-17 22:35:08 +0000

    add all required functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array) {
+    const newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+
+function countDown(num) {
+    for(let i = 0; i <= num; i++) {
+        console.log(i);
+    }
+}
